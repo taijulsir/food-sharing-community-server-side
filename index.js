93f8ec2003c280cf7facd1491fe174ae1c705294
taijulsir <princetaijul123@gmail.com>
2023-11-05 12:34:52 +0600

    Added get method to get all foods

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+
+
+    const foodsCollection = client.db('foodsDB').collection('foods')
+    app.get('/foods',async(req,res)=>{
+      const result = await foodsCollection.find().toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
