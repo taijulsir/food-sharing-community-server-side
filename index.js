733468d4fa0c067afb3a37bda37aca8f25f4ff04
taijulsir <princetaijul123@gmail.com>
2023-11-09 18:42:18 +0600

    delete unusual routes

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cookieParser = require("cookie-parser");
 const app = express()
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors({
   origin: [
-      'http://localhost:5173',
+      'http://localhost:5173','http://localhost:5174',
       'https://food-donation-community.web.app',
       'https://food-donation-community.firebaseapp.com/'
   ],
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser());
 
-
+// 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -56,11 +56,9 @@ const verifyToken = (req, res, next) => {
 
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
-
-
     const foodsCollection = client.db('foodsDB').collection('foods')
     const requestFoodsCollection = client.db('foodsDB').collection('requestFoods')
     const bannerCollection = client.db('foodsDB').collection('bannerImage')
@@ -73,18 +71,22 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res
           .cookie('token', token, {
-              httpOnly: true,
-              secure: false,
-              // sameSite: 'none'
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           })
           .send({ success: true })
+          
   })
 
   app.post('/logout', async (req, res) => {
       const user = req.body
       console.log('user hitten', user)
-      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+      res.clearCookie('token', { maxAge: 0,
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', }).send({ success: true })
   })
+
 
 
     // create add foods data
@@ -192,7 +194,7 @@ async function run() {
     })
 
     // create delete method to cancel any food item
-    app.delete('cancelFoods/:id',async(req,res)=>{
+    app.delete('/requestedFoods/:id',async(req,res)=>{
       const id = req.params.id;
       console.log(id)
       const query = {_id: new ObjectId (id)}
@@ -234,7 +236,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
